b818b622f2e33e12e68e6b53270aaa4faf1f867e
gsayson <geruls@broskiclan.org>
2022-12-17 22:42:06 +0800

    problems building on linux, to fix later

--- deacon-parse/src/lib.rs
+++ deacon-parse/src/lib.rs
@@ -1,13 +1,13 @@
-use nom::branch::alt;
-use nom::bytes::complete::take_while;
-use nom::character::complete::{alpha0, char};
-use nom::sequence::delimited;
-
-pub fn parse_env_vars(input: impl ToString) {
-    let input = input.to_string();
-    delimited(
-        char('$'),
-        alpha0,
-        char('$')
-    )(input).expect("TODO: panic message");
-}
+// use nom::branch::alt;
+// use nom::bytes::complete::take_while;
+// use nom::character::complete::{alpha0, char};
+// use nom::sequence::delimited;
+//
+// pub fn parse_env_vars(input: impl ToString) {
+//     let input = input.to_string();
+//     delimited(
+//         char('$'),
+//         alpha0,
+//         char('$')
+//     )(input).expect("panic message");
+// }
