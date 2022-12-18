f519ab4339d7f9799839c297c8bd4746893dca2b
gsayson <geruls@broskiclan.org>
2022-12-18 22:34:50 +0800

    let git integration check parent dirs

--- deacon/src/integrations/git.rs
+++ deacon/src/integrations/git.rs
@@ -3,33 +3,19 @@ use git2::{BranchType, Repository};
 use nerd_fonts::NerdFonts;
 
 pub fn is_git_project(dir: impl AsRef<Path>) -> bool {
-	Repository::open(dir.as_ref()).is_ok()
+	let dir = dir.as_ref();
+	Repository::open(dir).is_ok()
+	|| {
+		if let Some(parent) = dir.parent() {
+			is_git_project(dir)
+		} else {
+			false
+		}
+	}
 }
 
 pub fn get_integration(dir: impl AsRef<Path>) -> Option<String> {
 	let repo = Repository::open(dir.as_ref()).unwrap();
-	// let t = match repo.revwalk() {
-	// 	Ok(mut ok) => {
-	// 		let i = ok.next();
-	// 		if let Some(i) = i {
-	// 			match i {
-	// 				Ok(oid) => {
-	// 					let commit = repo.find_commit(oid).unwrap(); // this will never panic
-	// 					Some(format!("[{}: {}]", commit.author(), oid.to_string().substring(0, 6)))
-	// 				}
-	// 				Err(_) => {
-	// 					None
-	// 				}
-	// 			}
-	// 		} else {
-	// 			None
-	// 		}
-	// 	}
-	// 	Err(_) => {
-	// 		None
-	// 	}
-	// };
-	// t
 	let binding = repo.branches(Some(BranchType::Local)).ok()?.next()?.ok()?;
 	let br = binding.0.name().ok()??;
 	Some(
